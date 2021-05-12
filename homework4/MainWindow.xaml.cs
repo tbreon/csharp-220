eb525045ff384752b0749fdbd39d980413cebe5d
Travis <breontr@gmail.com>
2021-05-12 09:27:36 -0500

    homework 4 finished

---
+++ homework4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace homework4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string input = (sender as TextBox).Text;
+
+            if (!Regex.IsMatch(input, @"^(\d{5}|[A-Z]\d[A-Z] ?\d[A-Z]\d|\d{5}[-]\d{4})$"))
+            {
+                zipSubmit.IsEnabled = false;
+            }
+            else
+            {
+                zipSubmit.IsEnabled = true;
+            }
+        }
+
+    }
+}
